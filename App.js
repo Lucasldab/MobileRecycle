695e1de3565f1400079a85371795349e01897dae
Lucas Barros <43314760+Lucasldab@users.noreply.github.com>
2023-06-21 18:20:34 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import Home from './screens/home'
+import Identifier from './screens/identifier'
+import Map from './screens/recycleMap'
+import Info from './screens/info'
+import Rewards from './screens/rewards'
+import Schedule from './screens/schedule'
+import MarkerPage from './screens/markerPage'
+
+const Stack = createStackNavigator();
+
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+          name='Home'
+          component={Home}
+          />
+          <Stack.Screen
+          name='Identifier'
+          component={Identifier}
+          />
+          <Stack.Screen
+          name='Info'
+          component={Info}
+          options={{title: "Dicas de Reciclagem"}}
+          />
+          <Stack.Screen
+          name='Map'
+          component={Map}
+          />
+          <Stack.Screen
+          name='Rewards'
+          component={Rewards}
+          />
+          <Stack.Screen
+          name='Schedule'
+          component={Schedule}
+          />
+          <Stack.Screen
+          name='MarkerPage'
+          component={MarkerPage}
+          options={{title: "Ponto de Reciclagem"}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
